431dc41e5cff405b3afe365f5352cc3bb3e484d0
mabgcm <bugucam@gmail.com>
2023-11-13 08:37:33 -0500

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="grid place-items-center h-screen -mt-24">
+      <h1>Welcome Page</h1>
+    </div>
+  );
+}
---
+++ app/user/page.js
@@ -0,0 +1,13 @@
+import UserInfo from '@/components/UserInfo'
+import React from 'react'
+
+export default function Userpage() {
+    return (
+        <>
+            <div>
+                <h1>User Page</h1>
+            </div>
+            <UserInfo />
+        </>
+    )
+}
